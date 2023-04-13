9c290eb3f69a5e368c099fc3717a6cb1d4d3eaed
Rangelmath <130707510+Rangelmath@users.noreply.github.com>
2023-04-13 19:26:49 -0300

    Add files via upload

--- Cartorio Ebac switch.cpp
+++ Cartorio Ebac switch.cpp
@@ -148,44 +148,64 @@ int main ()
 {
     int opcao=0; //Definindo variáveis
 	int laco=1;
+	char senhadigitada [10]="a";
+	int comparacao; 
 	
-	for (laco=1;laco=1;)
+	printf("\t ### Cartório da EBAC ###\n\n"); //Inicio do menu
+	printf("Login de administrador!\n Digite a sua senha:");
+	scanf("%s",senhadigitada);
 	
-   	{
-	    system("cls");
-	    setlocale(LC_ALL,"Portuguese"); //Definindo linguagens 
+	comparacao = strcmp(senhadigitada, "admin");
+	
+	if(comparacao == 0)
+	{
+	
+    	for (laco=1;laco=1;)
+	
+    	{
+	        system("cls");
+	        setlocale(LC_ALL,"Portuguese"); //Definindo linguagens 
 	    
-	    printf("\t ### Cartório da EBAC ###\n\n"); //Inicio do menu
-	    printf("\t Escolha a opção desejada do menu:\n\n");
-	    printf("\t1 - Registrar nomes\n");
-	    printf("\t2 - Consultar nomes\n");
-	    printf("\t3 - Deletar nomes\n"); 
-	    printf("Opção: ");//Fim do menu
+	        printf("\t Escolha a opção desejada do menu:\n\n");
+	        printf("\t1 - Registrar nomes\n");
+	        printf("\t2 - Consultar nomes\n");
+	        printf("\t3 - Deletar nomes\n"); 
+	        printf("\t4 - Sair do sistema\n");
+	        printf("Opção: ");//Fim do menu
 	
-	    scanf("%d", &opcao); //Armazenamento da escolha do usuário
+     	    scanf("%d", &opcao); //Armazenamento da escolha do usuário
 	
-	    system("cls");
+    	    system("cls");
 	    
-        switch (opcao) 
-        {
+            switch (opcao) 
+            {
     	
-        case 1:
-	    registro ();
-	    break;
+                case 1:  
+       	        registro ();
+	            break;
 	    
-	    case 2:
-	    consultar ();
-	    break;
+        	    case 2:
+	            consultar ();
+	            break;
 	    
-	    case 3:
-        deletar ();
-        break; 
+        	    case 3:
+                deletar ();
+                break; 
         
-	    default:
-	    dft ();
-		break;
+                case 4:
+                printf("Obrigado por utilizar o sistema!");
+                return 0;
+        
+         	    default:
+        	    dft ();
+        		break;
  
         } //end switch
    } //end for
 } //end main
 
+else
+
+printf("Senha incorreta!");
+
+}
